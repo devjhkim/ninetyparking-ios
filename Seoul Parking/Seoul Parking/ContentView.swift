116697d43f106c4d9c7041c6335a88478aeb848b
Chiduk Song <chiduksong@gmail.com>
2020-07-09 15:13:40 +0900

    initial commit

---
+++ Seoul Parking/Seoul Parking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seoul Parking
+//
+//  Created by Chiduk Song on 7/9/20.
+//  Copyright © 2020 Chiduk Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
