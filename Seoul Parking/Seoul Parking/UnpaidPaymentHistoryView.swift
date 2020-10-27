2b6cf58cb7a44cff6bbd498b9966e248f516035e
Chiduk Song <chiduksong@gmail.com>
2020-10-27 11:07:50 +0900

    commit

---
+++ Seoul Parking/Seoul Parking/UnpaidPaymentHistoryView.swift
@@ -0,0 +1,21 @@
+//
+//  UnpaidPaymentHistoryView.swift
+//  Seoul Parking
+//
+//  Created by Chiduk Song on 2020/10/27.
+//  Copyright © 2020 Chiduk Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct UnpaidPaymentHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnpaidPaymentHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnpaidPaymentHistoryView()
+    }
+}
